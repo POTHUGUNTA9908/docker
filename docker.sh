daeef877ecbd37f747bb2941aa3c9a84d0a29259
pothuguntarakesh <pothugunta.rakesh.com>
2026-03-21 09:30:36 +0530

    expense

--- docker.sh
+++ docker.sh
@@ -28,3 +28,8 @@ mv /tmp/eksctl /usr/local/bin
 
 
 
+#helm installation
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
